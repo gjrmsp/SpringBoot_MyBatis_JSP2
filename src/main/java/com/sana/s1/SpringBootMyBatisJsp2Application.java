cbb134c1c271573ece88ccff5f6d8aa3fcce93e1
gjrmsp <gjrmsp@naver.com>
2021-05-20 17:06:41 +0900

    Initial commit

---
+++ src/main/java/com/sana/s1/SpringBootMyBatisJsp2Application.java
@@ -0,0 +1,13 @@
+package com.sana.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisJsp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyBatisJsp2Application.class, args);
+	}
+
+}
