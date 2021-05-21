bc160a669bfd8986849cf341b12a117428f47a57
gjrmsp <gjrmsp@naver.com>
2021-05-21 16:50:34 +0900

    update

--- src/main/java/com/sana/s1/member/MemberVO.java
+++ src/main/java/com/sana/s1/member/MemberVO.java
@@ -6,7 +6,9 @@ import javax.validation.constraints.Min;
 //import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 //import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+//import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
 
@@ -16,7 +18,7 @@ public class MemberVO {
 	private String username;
 
 	//최소 4글자 이상
-	@Size(min = 4)
+	@Length(min = 6,max = 12)
 	private String password;
 
 	@NotEmpty(message = "넌 이름이 없니??") //name은 null이 아니여야 한다는 의미
@@ -25,6 +27,8 @@ public class MemberVO {
 	@Email
 	@NotEmpty
 	private String email;
+	
+	@NotEmpty
 	private String phone;
 
 	//0 이상 200이하
